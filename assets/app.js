bd99c911b4e3505f24eb80d0c20bde3664e7f752
MortyOW <adam.simon@edu.devinci.fr>
2023-03-09 09:51:39 +0100

    :sparkles: MAP added

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,33 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+function initMap() {
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+        iconRetinaUrl: require('/images/marker-icon-2x.png'),
+        iconUrl: require('/images/marker-icon.png'),
+        shadowUrl: require('/images/marker-shadow.png')
+    });
+
+    let map = L.map('map').setView([48.780718, 2.266203], 40);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    let marker = L.marker([48.780718, 2.266203]).addTo(map);
+    marker.bindPopup("<b>Hello world!</b><br><a href='#'>I am a popup</a>.").openPopup();
+}
+
+window.addEventListener('load', initMap);
+
--- webpack.config.js
+++ webpack.config.js
@@ -74,3 +74,5 @@ Encore
 ;
 
 module.exports = Encore.getWebpackConfig();
+
+
