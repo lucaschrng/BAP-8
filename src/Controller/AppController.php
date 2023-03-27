2e4aa0fc5e1f865cc54723820c0fcc37f9ae54af
Tony Zhang <zhgtony.pro@hotmail.com>
2023-03-27 16:11:42 +0200

    :lipstick: wip front

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -11,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_app')]
-    public function index(TypeRepository $typeRepository): Response
+    public function index(TypeRepository $typeRepository, LocationRepository $locationRepository ): Response
     {
         return $this->render('app/index.html.twig', [
             'types' => $typeRepository->findAll(),
+            'locations' => $locationRepository->findAll(),
         ]);
     }
 }
