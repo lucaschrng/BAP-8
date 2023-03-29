c51f36d22cf3c50a8bf24040d5a96d04265a7c78
Tony Zhang <zhgtony.pro@hotmail.com>
2023-03-29 17:07:15 +0200

    wip responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,16 @@ module.exports = {
     fontFamily : {
       sans: ['bely-display', 'sans-serif'],
     },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1500px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 }
