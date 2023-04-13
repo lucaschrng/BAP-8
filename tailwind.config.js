76f77104e9f4e39936786ee31000e2995a2dc767
R_K <shawnfroste@outlook.fr>
2023-04-13 08:31:57 +0200

    first page integration for 1st step

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,25 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily : {
-        sans: ['bely-display', 'sans-serif'],
-        txt: ['finalsix', 'sans-serif'],
-      },
+      colors: {
+        colorGreen: '#1A6E6A',
+        colorOrange : "#FC6B3B",
+        colorBlueGreen : "#19647E"
+      }
+    },
+    fontFamily : {
+      sans: ['bely-display', 'sans-serif'],
+      txt: ['finalsix', 'sans-serif'],
+    },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1500px',
+      // => @media (min-width: 1280px) { ... }
     },
   },
   plugins: [],
